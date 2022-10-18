f2ed29f46a858ac00310c3473a222a47131238ad
Carlos Serôdio <carlosserodio86@hotmail.com>
2022-10-18 16:49:35 +0100

    Product Details

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,28 @@
 /****************************** imports ********************************/
-
 import Layout from "../components/Layout";
 import ProductItem from "../components/ProductItem";
 import data from "../utils/data";
-
 /***************************** End of imports **************************/
 
 /***************** Home - React functional Component *****************/
-
 export default function Home() {
   /******************************* return *********************************/
-
   return (
+    /****************************** Layout ********************************/
     <Layout title='Home Page'>
-      {/* className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4" */}
+      {/****************************** div ********************************/}
       <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
+        {/* map products */}
         {data.products.map((product) => (
+          // render product by slug
           <ProductItem product={product} key={product.slug}></ProductItem>
         ))}
       </div>
+      {/**************************** div end ******************************/}
     </Layout>
+    /****************************** end of Layout *************************/
   );
 
   /****************************** End of return ***************************/
 }
-
 /*************** End of Home - React functional Component ************/
